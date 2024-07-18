62ba75578d02c4d828b7f19a57dfa5c486d0b0e6
YungLust <artyr.yanchuk2@gmail.com>
2024-07-18 14:58:38 +0300

    Added recurrent factorial method

---
+++ Recursion.java
@@ -0,0 +1,15 @@
+class Factorial {
+    long fact(int n){
+        long res;
+        if (n == 1) return 1;
+        res = fact(n-1) * n;
+        return res;
+    }
+}
+class Recursion {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        int n = 12;
+        System.out.println("Factorial of " + n + " = " + f.fact(n));
+    }
+}
