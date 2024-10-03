a040a90cb2d4d8c9c75a43d4b9f86b059da38bce
YungLust <artyr.yanchuk2@gmail.com>
2024-10-03 22:41:08 +0300

    Added comments, fixed import, added break in NEVER case

--- AskMe.java
+++ AskMe.java
@@ -1,11 +1,11 @@
 import java.util.Random;
-import java.util.random.*;
 
 enum Answers{
     NO,YES,PROBABLY,SOON,LATER,NEVER,KYS,DONT
 }
 class Question{
     Random rand = new Random();
+    //we create method with the name 'ask' that returns 'Answers'
     Answers ask (){
         int randomInt = rand.nextInt(100);
         int randomInt1 = rand.nextInt(100);
@@ -36,6 +36,9 @@ class Question{
     }
 }
 class AskMe {
+    //we create method that is available without creating class object
+    //it takes Answers varialbe
+    //that we take from Question`s class method - 'ask'
     static void answer (Answers answer){
         switch (answer) {
             case YES:
@@ -55,6 +58,7 @@ class AskMe {
                 break;
             case NEVER:
                 System.out.println("Never.");
+                break;
             case DONT:
                 System.out.println("Don`t ask me. I don`t know!");
                 break;
