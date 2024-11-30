17901fa7849caf02a86dd3c49979b8de234b18d0
YungLust <artyr.yanchuk2@gmail.com>
2024-11-30 21:37:56 +0200

    Main

---
+++ ShapesDrawing/Main.java
@@ -0,0 +1,29 @@
+package ShapesDrawing;
+/**
+ * In main class we create rectangles from different figures and draw them
+ */
+
+class Main{
+    public static void main(String[] args){
+
+        //initialize our figures
+        RoundedRectangle roundedRect = new RoundedRectangle(4,8,25);
+        Polygon polygon = new Polygon(3, 12);
+        Rectangle rect = new Rectangle(12,6);
+
+        //adapt if needed
+        RectangleAdapter roundedAdapter = new RectangleAdapter(roundedRect);
+        RectangleAdapter polygonAdapter = new RectangleAdapter(polygon);
+
+        //draw figures
+        DrawRectangle drawRect = new DrawRectangle(roundedAdapter);
+        drawRect.draw();
+        System.out.println("\n");
+        drawRect.setRectangle(polygonAdapter);
+        drawRect.draw();
+        System.out.println("\n");
+        drawRect.setRectangle(rect);
+        drawRect.draw();
+
+    }
+}
